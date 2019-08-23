1ce6c8fedaf9bcc3a6623e281ba384bdda21dc5f
arnie b <arnold.bwaila@gmail.com>
2019-08-23 14:52:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,11 @@ def reverse_each_word(string)
   str_arry = string.split
   reversed = []
   newstring =""
-  
+  # str_arry.map do |elemnt|
+  # reversed << elemnt.reverse
+  # end
+  # newstring = reversed.join(" ")
+  #newstring
  str_arry.collect do |elemnt|
    reversed << elemnt.reverse
   end
