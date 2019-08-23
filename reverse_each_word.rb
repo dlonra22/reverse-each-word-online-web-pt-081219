4250bbc6031067043fc5d85a07c2a084701d75d0
arnie b <arnold.bwaila@gmail.com>
2019-08-23 11:00:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   str_arry = string.split
   reversed = []
   str_arry.each do |elemnt|
-   reversed << elemnt.reversed
+   reversed << elemnt.reverse!
    puts reversed
   end
  end
